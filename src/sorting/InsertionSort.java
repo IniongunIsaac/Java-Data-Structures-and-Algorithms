db7a41efb034824440d6d9c391b67c66ac83a30c
Isaac Iniongun <iniongunisaac@gmail.com>
2023-04-03 20:18:28 +0100

    Implement insertion sort

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+package sorting;
+
+import java.util.Arrays;
+
+import static utils.Utils.intArray;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] sortedArray = sort();
+        System.out.println(Arrays.toString(sortedArray));
+    }
+
+    private static int[] sort() {
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+            int newElement = intArray[firstUnsortedIndex];
+            int i;
+            for(i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
+            }
+            intArray[i] = newElement;
+        }
+        return intArray;
+    }
+}

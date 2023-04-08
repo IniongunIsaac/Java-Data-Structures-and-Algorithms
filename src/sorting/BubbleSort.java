def20dc9eaa4c0d5b6539a69f5c4ac35f3f05ab0
Isaac Iniongun <iniongunisaac@gmail.com>
2023-04-08 18:04:48 +0100

    recursive insertion sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -4,6 +4,33 @@ import java.util.Arrays;
 import static utils.Utils.intArray;
 import static utils.Utils.swap;
 
+/**
+ * Bubble sort is a simple sorting algorithm that repeatedly steps through a list of items to be sorted, compares each pair of adjacent items and swaps them if they are in the wrong order. This process is repeated until no more swaps are needed, indicating that the list is now sorted.
+ *
+ * Here's how the algorithm works:
+ *
+ * Start with an unsorted list of items to be sorted.
+ * Set a flag to indicate whether any swaps have been made during the current iteration.
+ * Repeat the following steps until no more swaps are needed:
+ * a. For each pair of adjacent items in the list:
+ * i. If the items are in the wrong order, swap them.
+ * ii. Set the flag to indicate that a swap has been made.
+ * Stop when no more swaps are needed. The list is now sorted.
+ * Here's an example of bubble sort in action:
+ *
+ * Suppose we have an unsorted list of numbers: 7, 3, 5, 1, 9.
+ *
+ * Start with the first pair of adjacent items, (7, 3). Since 7 is greater than 3, we swap them. The list is now: 3, 7, 5, 1, 9.
+ * Set the flag to indicate that a swap has been made.
+ * Repeat step 1 with the next pair of adjacent items, (7, 5). Since 7 is greater than 5, we swap them. The list is now: 3, 5, 7, 1, 9.
+ * Set the flag to indicate that a swap has been made.
+ * Repeat step 1 with the next pair of adjacent items, (7, 1). Since 7 is greater than 1, we swap them. The list is now: 3, 5, 1, 7, 9.
+ * Set the flag to indicate that a swap has been made.
+ * Repeat step 1 with the next pair of adjacent items, (7, 9). Since 7 is not greater than 9, we don't swap them. The list remains: 3, 5, 1, 7, 9.
+ * Set the flag to indicate that no swaps have been made during this iteration.
+ * Since the flag is set to indicate that swaps have been made during the current iteration, we repeat the entire process again, starting with the first pair of adjacent items.
+ * We repeat this process until no more swaps are needed. In this case, the list is sorted after the third iteration: 1, 3, 5, 7, 9.
+*/
 public class BubbleSort {
 
     public static void main(String[] args) {
--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -6,8 +6,9 @@ import static utils.Utils.intArray;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] sortedArray = sort();
-        System.out.println(Arrays.toString(sortedArray));
+        //int[] sortedArray = sort();
+        //System.out.println(Arrays.toString(sortedArray));
+        recursiveSort(intArray, intArray.length);
     }
 
     //O(n^2)
@@ -21,5 +22,24 @@ public class InsertionSort {
             intArray[i] = newElement;
         }
         return intArray;
+    }
+
+    private static void recursiveSort(int[] input, int numItems) {
+        if (numItems < 2) {
+            return;
+        }
+
+        recursiveSort(input, numItems - 1);
+
+        int newElement = input[numItems - 1];
+        int i;
+        for(i = numItems - 1; i > 0 && input[i - 1] > newElement; i--) {
+            input[i] = input[i - 1];
+        }
+        input[i] = newElement;
+
+        System.out.println("Result when numItems = " + numItems);
+        System.out.println(Arrays.toString(input));
+        System.out.println("-------------------");
     }
 }

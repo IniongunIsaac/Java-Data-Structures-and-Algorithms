ea020f7dc43febe5a5d995f5194b97361a7e1088
Isaac Iniongun <iniongunisaac@gmail.com>
2023-04-09 18:46:09 +0100

    singly linked lists

---
+++ src/linkedlists/LinkedListMain.java
@@ -0,0 +1,29 @@
+package linkedlists;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        Employee jabe  = new Employee("Jabe", "Jogn", 123);
+        Employee john  = new Employee("John", "Jones", 456);
+        Employee kate  = new Employee("Kate", "Ingya", 789);
+        Employee karim  = new Employee("Usman", "Zico", 102);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+
+        System.out.println(list.isEmpty());
+
+        list.addToFront(jabe);
+        list.addToFront(john);
+        list.addToFront(kate);
+        list.addToFront(karim);
+
+        System.out.println(list.getSize());
+
+        list.printList();
+
+        System.out.println(list.removeFromFront());
+
+        System.out.println(list.getSize());
+
+        list.printList();
+    }
+}
---
+++ src/linkedlists/ListsMain.java
@@ -0,0 +1,23 @@
+package linkedlists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListsMain {
+    public static void main(String[] args) {
+        List<Employee> employeeList = new ArrayList<>();
+
+        employeeList.add(new Employee("Jabe", "Jogn", 123));
+        employeeList.add(new Employee("Jabe", "Jogn", 123));
+        employeeList.add(new Employee("Jabe", "Jogn", 123));
+        employeeList.add(new Employee("Jabe", "Jogn", 123));
+
+        employeeList.forEach(employee -> System.out.println(employee));
+
+        Employee[] employees = employeeList.toArray(new Employee[0]);
+
+        for (Employee employee: employees) {
+            System.out.println(employee);
+        }
+    }
+}
